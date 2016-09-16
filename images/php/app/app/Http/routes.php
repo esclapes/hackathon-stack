08a6156b03f04d92ff9ab790fca40ee877ed09fd
eduardo <eduardo.esclapes@gmail.com>
2016-09-16 15:27:22 +0200

    lumen query endpoint

--- images/php/app/app/Http/routes.php
+++ images/php/app/app/Http/routes.php
@@ -2,7 +2,7 @@
 
 use GuzzleHttp\Client;
 
-define('COOSTO_API')
+define('COOSTO_API_HOST', 'https://edu-coosto.dev.coosto.nl');
 
 $app->get('/', function () use ($app) {
     return $app->version();
@@ -10,13 +10,16 @@ $app->get('/', function () use ($app) {
 
 $app->get('/query/{term}', function($term) use ($app) {
 
-    $client = new Client();
+    $client = new Client(['base_uri' => COOSTO_API_HOST]);
 
-    $response = $client->request('GET', COOSTO_API, [
-        'sessionid' => COOSTO_TOKEN,
-        'term' => $term
+    $key = $app->request->input('sessionid');
+
+    $response = $client->request('GET', '/api/1/query/results', [
+        'query' => [
+            'sessionid' => $key,
+            'q' => $term
+        ]
     ]);
-
 
     return $response;
 });
